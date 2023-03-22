37155d83915b171aac3356d88983e8389f74601c
rknm-cell <racknahm@gmail.com>
2023-03-22 12:45:07 -0400

    created for loop to return value of the key of year

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+
+function superbowlWin(record){
+    //This function is a for loop, iterating through an array of record
+    //check for the result of W, if true returns the year.
+    for (let superbOwl  of record) {
+        if (superbOwl.result == "W") {
+            let winYear = superbOwl.year;
+            return winYear; 
+        };
+    };
+};
